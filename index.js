6bb5f2cd47425bb6fa6701e7c6256427bc76ae2a
sonas939 <70652191+sonas939@users.noreply.github.com>
2023-11-08 13:25:08 -0600

    fixed gitignore

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+require("dotenv").config(); 
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+mongoose.connect(
+    process.env.MONGODB_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
 
 app.listen(3001, () => 
     console.log("Server is running")
